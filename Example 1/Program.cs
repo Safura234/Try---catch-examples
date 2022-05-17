6c1629c115cdc279836ce03f2988c460b154e4b6
Safura234 <safuramg@code.edu.az>
2022-05-17 13:13:32 +0400

    https://github.com/Safura234/Try---catch-examples.git

---
+++ Example 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Find cube!");
+                Console.WriteLine("Enter a number");
+                var num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Cube of {num} is {num * num * num}");
+            }
+            catch
+            {
+                Console.Write(Console.Error);
+                  }
+            finally
+            {
+                Console.WriteLine("Re-enter just a number!");
+                Console.ReadLine();
+            }
+    }
+    }
+}
